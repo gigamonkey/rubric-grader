079f302e163c683ab48766b9fc4f78c4c7425369
Peter Seibel <peter@gigamonkeys.com>
2025-03-08 08:59:51 -0800

    Use functions from data in reload-rubric

--- reload-rubric.js
+++ reload-rubric.js
@@ -1,29 +1,12 @@
 #!/usr/bin/env node
 
-import YAML from 'yaml';
-import fs from 'fs';
 import path from 'path';
-import { glob } from 'glob';
 import { DB } from './db.js';
 import { argv } from 'process';
-
-const loadRubric = (db, rubric) => {
-  Object.entries(rubric).forEach(([question, items], sequence) => {
-    db.insertQuestion({sequence, question});
-    Object.entries(items).forEach(([criteria, weight], sequence) => {
-      db.insertRubricItem({question, criteria, sequence, weight});
-    });
-  });
-};
-
-const reloadRubric = (db, rubric) => {
-  db.clearQuestions();
-  db.clearRubric();
-  loadRubric(db, rubric);
-};
+import { reloadRubric } from './data.js'
 
 const dir = argv[2];
 const db = new DB(path.join(dir, 'db.db'), 'schema.sql');
-const rubric = YAML.parse(fs.readFileSync(path.join(dir, 'rubric.yml'), 'utf8'));
+const rubricFile = path.join(dir, 'rubric.yml')
 
-reloadRubric(db, rubric);
+reloadRubric(db, rubricFile);
